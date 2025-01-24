8ebdf98a4d6e034a320ca37070e821009f14de0d
bao1311 <105867837+bao1311@users.noreply.github.com>
2025-01-24 07:55:48 -0700

    Change 802.py to 2 solutions: Kahn's algo and graph marking

--- 802/802.py
+++ 802/802.py
@@ -1,32 +1,68 @@
+# Solve by using both Kahn's algo and graph marking
+
 class Solution:
     def eventualSafeNodes(self, g: List[List[int]]) -> List[int]:
         # if can reach the terminal node -> safe node
         # safe if every possible path start from that nodes lead to a terminal node
 
+        # Kahn's algo
+        n = len(g)
+        out = [0] * n
+        O = [[] for _ in range(n)]
+        for i in range(n):
+            for nei in g[i]:
+                O[nei].append(i)
+                out[i] += 1
+        ret = []
+        q = deque()
+        for i in range(n):
+            if out[i] == 0:
+                q.append(i)
+                ret.append(i)
+
+        while q:
+            t = q.popleft()
+            for nei in O[t]:
+                out[nei] -= 1
+                if out[nei] == 0:
+                    ret.append(nei)
+                    q.append(nei)
+                
+        return sorted(ret)
+
+
         # have to mark the node
         # optimal dfs
 
-        n = len(g)
+        # n = len(g)
 
-        c = [0] * n
+        # c = [0] * n
 
-        # 0: not visited
-        # 1: works fine (safe)
-        # 2: cause cycle
-        def dfs(i):
-            if c[i] != 0:
-                return c[i] == 1
-            c[i] = 2 # put it here 
-            # use backtrack
-            for n in g[i]:
-                if not dfs(n):
-                    return False
+        # # 0: not visited
+        # # 1: works fine (safe)
+        # # 2: cause cycle
+        # def dfs(i):
+        #     if c[i] != 0:
+        #         return c[i] == 1
+        #     c[i] = 2 # put it here 
+        #     # use backtrack
+        #     for n in g[i]:
+        #         if not dfs(n):
+        #             return False
             
-            c[i] = 1
-            return True
-        ret = []
-        for i in range(n):
-            if dfs(i):
-                ret.append
+        #     c[i] = 1
+        #     return True
+        # ret = []
+        # for i in range(n):
+        #     if dfs(i):
+        #         ret.append
             
-        return ret
+        # return ret
+
+
+
+
+
+
+
+
